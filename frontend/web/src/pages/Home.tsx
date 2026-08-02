9a7dcc99ce4fefa02c901fd274e7f3a691901052
Wishwa <jananawishwa69@gmail.com>
2026-08-02 20:32:57 +0530

    Add PlantAI home page

---
+++ frontend/web/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to PlantAI</h1>
+      <p>AI-powered plant health assistant</p>
+    </div>
+  );
+}
+
+export default Home;
